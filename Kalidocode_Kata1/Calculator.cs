24bebb3abe8abe8a45d73bcbe9c3b336fae68e31
TanyaGovender <tcgovender2003@gmail.com>
2025-03-25 22:31:05 +0200

    Functionallity added to split string input based on accepted delimiters and then add all numbers in split array

--- Kalidocode_Kata1/Calculator.cs
+++ Kalidocode_Kata1/Calculator.cs
@@ -17,8 +17,19 @@ namespace Kalidocode_Kata1
 
             if (string.IsNullOrEmpty(input) || input.Contains(" ")) { return 0; } // null input >> automatically sum = 0
             if (input.Contains(",,") || input.Contains(",\\n") || input.Contains("\\n,")) { throw new Exception("Incorrect format of numbers"); }
-
-            try { sum = int.Parse(input); return sum; } catch { return -1; }
+            if (input.Contains(",") || input.Contains(";") || input.Contains("\\n"))
+            {
+                string[] arrNumbers = input.Split(new string[] {",", ";", "\\n"},StringSplitOptions.RemoveEmptyEntries );
+                foreach (string number in arrNumbers)
+                {   int num = int.Parse(number);
+                    if (num >= 0) { if (num < 1000) { sum += num; } }
+                    else { errorMessage += $"Negatives not allowed: {num}\n"; throw new Exception(errorMessage); }
+                } return sum;
+            }
+            try { sum = int.Parse(input); 
+                if (sum >= 0) { if (sum < 1000) {return sum; } }
+                errorMessage += $"Negatives not allowed: {sum}\n"; throw new Exception(errorMessage);} 
+            catch { return -1; }
            
         }
     }
