ed06e6eeb4258fbb26dcb58a62c3187f721f7082
TanyaGovender <tcgovender2003@gmail.com>
2025-03-25 22:04:13 +0200

    Functionallity added to return 0 when given a null input

---
+++ CalculateTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculateTest
+{
+    public class Tests
+    {
+       
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Kalidocode_Kata1/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalidocode_Kata1
+{
+    class Calculator
+    {
+        public static string errorMessage = "";
+
+        public int Add(string? input)
+        {
+            List<int> numbers = new List<int>();
+            int length = input.Length; int sum = 0;
+
+            if (input == null || input.Contains(" ") || input.Contains("")) { return 0; } // null input >> automatically sum = 0
+            if (input.Contains(",,") || input.Contains(",\\n") || input.Contains("\\n,")) { throw new Exception("Incorrect format of numbers"); }
+            return -1;
+        }
+    }
+}
