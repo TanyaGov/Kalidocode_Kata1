827984ea3fc0ccf97e1fcab6f812145375d5bea2
TanyaGovender <tcgovender2003@gmail.com>
2025-03-27 15:17:27 +0200

    Test created for Requirment 2 and 3

--- CalculateTest/UnitTest1.cs
+++ CalculateTest/UnitTest1.cs
@@ -29,6 +29,77 @@ namespace CalculateTest
             Assert.That(sum, Is.EqualTo(0));
         }
 
+        [Test]
+        public void Given7_WhenAdded_Returns7()
+        {
+            // Arrange
+            var input = "7";
+            Calculator calculator = new Calculator();
+            // Act
+            var sum = calculator.Add(input);
+            //Assert
+            Assert.That(sum, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void GivenCommasOnly_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            var input = "8,2,20";
+            Calculator calculator = new Calculator();
+            // Act
+            var sum = calculator.Add(input);
+            //Assert
+            Assert.That(sum, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void GivenSlashNOnly_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            var input = "8\n2\n20";
+            Calculator calculator = new Calculator();
+            // Act
+            var sum = calculator.Add(input);
+            //Assert
+            Assert.That(sum, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void GivenBothSlashNOnlyAndCommas_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            var input = "8\n2,20";
+            Calculator calculator = new Calculator();
+            // Act
+            var sum = calculator.Add(input);
+            //Assert
+            Assert.That(sum, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void GivenSingleCustomDelimiter_WhenAdded_ReturnsSum()
+        {
+            //Arrange
+            var input = "//%\n87%11%3";
+            Calculator calculator = new Calculator();
+            //Act
+            var sum = calculator.Add(input);
+            //Assert
+            Assert.That(sum, Is.EqualTo(101));
+        }
+
+        [Test]
+        public void GivenLongDelimiter_WhenAdded_ReturnsSum()
+        {
+            //Arrange
+            var input = "//***\n150***22***400";
+            Calculator calculator = new Calculator();
+            //Act
+            var sum = calculator.Add(input);
+            //Assert
+            Assert.That(sum, Is.EqualTo(572));
+        }
 
     }
 }
