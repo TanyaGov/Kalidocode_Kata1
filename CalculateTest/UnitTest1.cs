9bb8f49ecaa6247b0c643f4c4f24e4f29fbf3d4e
TanyaGovender <tcgovender2003@gmail.com>
2025-03-27 15:08:22 +0200

    Test for Requirment 1

--- CalculateTest/UnitTest1.cs
+++ CalculateTest/UnitTest1.cs
@@ -6,17 +6,29 @@ namespace CalculateTest
     {
 
         [Test]
-        public void GivenNull_WhenAdded_ReturnsZero()
+        public void GivenEmpty_WhenAdded_ReturnsZero()
         {
             // Arrange
             string input = " ";
             Calculator calculator = new Calculator();
-
             // Act
             var sum = calculator.Add(input);
-
             // Assert
             Assert.That(sum, Is.EqualTo(0));
         }
+
+        [Test]
+        public void GivenNull_WhenAdded_ReturnsZero()
+        {
+            // Arrange
+            string input = null;
+            Calculator calculator = new Calculator();
+            // Act
+            var sum = calculator.Add(input);
+            // Assert
+            Assert.That(sum, Is.EqualTo(0));
+        }
+
+
     }
 }
