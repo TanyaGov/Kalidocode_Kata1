a9f3eb10f1446aade3dda3e329fd8bdc6aeea7bf
TanyaGovender <tcgovender2003@gmail.com>
2025-03-27 11:52:46 +0200

    Receive input

--- Kalidocode_Kata1/Program.cs
+++ Kalidocode_Kata1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Kalidocode_Kata1
+{
+    public class Program
+    {
+      
+        public static void Main(string[] args)
+        {
+            string line = ""; string input;
+
+            while ((input = Console.ReadLine()) != null)
+            { line += input; }
+
+            Console.WriteLine(line);
+        }
+
+    }
+}
