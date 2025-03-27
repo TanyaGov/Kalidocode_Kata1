f5fad7eb65a7e9323fa8895e01a8517dc2444630
TanyaGovender <tcgovender2003@gmail.com>
2025-03-27 10:34:03 +0200

    New Project Created - All old code removed

--- Kalidocode_Kata1/Calculator.cs
+++ Kalidocode_Kata1/Calculator.cs
@@ -8,62 +8,6 @@ namespace Kalidocode_Kata1
 {
     public class Calculator
     {
-        public static string errorMessage = "";
 
-        public int Add(string? input)
-        {
-            List<int> numbers = new List<int>();
-            int length = input.Length; int sum = 0;
-
-            if (string.IsNullOrEmpty(input) || input.Contains(" ")) { return 0; } // null input >> automatically sum = 0
-            if (input.Contains(",,") || input.Contains(",\\n") || input.Contains("\\n,")) { throw new Exception("Incorrect format of numbers"); }
-            if (input.Contains(",") || input.Contains(";"))
-            {
-                string[] arrNumbers = input.Split(new string[] {",", ";", "\\n"},StringSplitOptions.RemoveEmptyEntries );
-                foreach (string number in arrNumbers)
-                {   int num = int.Parse(number);
-                    if (num >= 0) { if (num < 1000) { sum += num; } }
-                    else { errorMessage += $"Negatives not allowed: {num}\n"; throw new Exception(errorMessage); }
-                } return sum;
-            }
-            if (input.Contains("//")) { sum = CustomDelimiters(input); return sum; }
-            try { sum = int.Parse(input); 
-                if (sum >= 0) { if (sum < 1000) {return sum; } }
-                errorMessage += $"Negatives not allowed: {sum}\n"; throw new Exception(errorMessage);} 
-            catch { return -1; }
-        }
-
-        public int CustomDelimiters(string input)
-        {
-            int sum = 0;
-            int indexOfN = input.IndexOf("n");
-            string stringNumbers = input.Remove(0, indexOfN + 1);
-
-            if (input.Contains("["))
-            {
-                int index = input.LastIndexOf(']'); string stringDelimiters = input.Substring(2, index + 1);
-                string[] arrDelimiters = stringDelimiters.Split(new string[] { "[", "]" }, StringSplitOptions.RemoveEmptyEntries);
-                string[] arrNumbers = stringNumbers.Split(arrDelimiters, StringSplitOptions.RemoveEmptyEntries);
-                foreach (string number in arrNumbers)
-                {
-                    int num = int.Parse(number); if (num >= 0) { if (num < 1000) { sum += num; } }
-                    else { errorMessage += $"Negatives not allowed: {num}\n"; throw new Exception(errorMessage); }
-                }
-
-            }// multiple custom delimiters
-            else
-            {
-                string delimiter = input.Substring(2, (indexOfN - 3));
-                string[] arrNumbers = stringNumbers.Split(delimiter);
-                foreach (string number in arrNumbers)
-                {
-                    int num = int.Parse(number); if (num >= 0) { if (num < 1000) { sum += num; } }
-                    else { errorMessage += $"Negatives not allowed: {num}\n"; throw new Exception(errorMessage); }
-                }
-                Console.WriteLine(sum);
-            }
-
-            return sum;
-        }
     }//class
 }//ns
--- Kalidocode_Kata1/Program.cs
+++ Kalidocode_Kata1/Program.cs
@@ -2,17 +2,9 @@
 {
     public class Program
     {
-        public static string? input = null;
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a String containing the numbers you want to add, seperated by a character, such as ','");
-            input = Console.ReadLine();
-
-            Calculator calculator = new Calculator();
-
-            int sum = calculator.Add(input);
-            Console.WriteLine($"Sum is: {sum}");
-
+            
         }
     }
 }
