55a56be75a15d7f02d378e57af5125a0fb81ac89
TanyaGovender <tcgovender2003@gmail.com>
2025-03-28 10:04:51 +0200

    Creating Attempt 3 - Removed All old code

--- Kalidocode_Kata1/Program.cs
+++ Kalidocode_Kata1/Program.cs
@@ -5,18 +5,7 @@
       
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter numbers:");
-
-            string line = ""; 
-            string input;
-
-            while ((input = Console.ReadLine()) != null)
-            { line += input + "\n"; }
-
-            Calculator calculator = new Calculator();   
-            int sum = calculator.Add(line);
-            Console.WriteLine(sum);
-            
+           
         }
 
     }
