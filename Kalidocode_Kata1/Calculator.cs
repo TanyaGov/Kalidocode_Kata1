dca56a599bae0a338f7cd9e74edfa429b4ffe751
TanyaGovender <tcgovender2003@gmail.com>
2025-03-28 21:21:32 +0200

    Functionallity added to meet requirment 1

--- Kalidocode_Kata1/Calculator.cs
+++ Kalidocode_Kata1/Calculator.cs
@@ -2,6 +2,29 @@
 {
     public class Calculator
     {
+        public int Add(string input)
+        {
+            string[] standardDelimiters = { ",", "\n" };
 
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+
+            if (standardDelimiters.Any(input.Contains))
+            {
+                int sum = 0;
+                string[] arrNumbers = input.Split(standardDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string number in arrNumbers) 
+                {
+                    sum += Convert.ToInt32(number);
+                }
+
+                return sum;
+            }
+
+            return Convert.ToInt32(input);
+        }
     }
 }
--- Kalidocode_Kata1/Program.cs
+++ Kalidocode_Kata1/Program.cs
@@ -4,7 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("hello");
+            string input = "";
+            string line = "";
+
+            Console.WriteLine("Please enter numbers:");
+
+            while ((input = Console.ReadLine()) != null)
+            {
+                line += input + "\n";
+            }
+
+            Calculator calculator = new Calculator();
+            int sum = calculator.Add(line);
+            Console.WriteLine($"Sum of numbers is: {sum}");
         }
     }
 }
