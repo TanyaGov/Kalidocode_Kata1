a2cab302b764490c846ebaa3c421b1ef12854213
TanyaGovender <tcgovender2003@gmail.com>
2025-03-28 10:12:40 +0200

    Old Project Removed

--- Kalidocode_Kata1/Calculator.cs
+++ Kalidocode_Kata1/Calculator.cs
@@ -8,6 +8,69 @@ namespace Kalidocode_Kata1
 {
     public class Calculator
     {
-       
+        public int Add(string input)
+        {
+            int sum = 0;
+            string error = "Negative numbers:";
+
+            if (string.IsNullOrEmpty(input) || input.Contains(" ")) { sum = 0; return sum; }
+
+            if (input.Contains("//"))
+            {
+                int beforeSlash = 2;
+                int indexNextLine = input.IndexOf("\n");
+                string delimiterString = input.Substring(beforeSlash, (indexNextLine - beforeSlash));
+                string numbersString = input.Substring(indexNextLine + 1);
+
+                if (delimiterString.Contains("[") && delimiterString.Contains("]")) 
+                {
+                    string[] arrDelimiters = delimiterString.Split(new string[] { "[", "]" }, StringSplitOptions.RemoveEmptyEntries);
+                    string[] arrNumbers = numbersString.Split(arrDelimiters, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string number in arrNumbers)
+                    {
+                        int num = int.Parse(number);
+                        if (num >= 0 && num < 1000) { sum += num; }
+                        else if (num < 0) { error += "\t" + number; }
+                    }
+
+                    if (!error.Equals("Negative numbers:")) { throw new Exception(error); }
+                    return sum;
+                }
+                else
+                {
+                    string[] arrNumbers = numbersString.Split(delimiterString);
+                    foreach (string number in arrNumbers)
+                    {
+                        int num = int.Parse(number);
+                        if (num >= 0 && num < 1000) { sum += num; }
+                        else if (num < 0) { error += "\t" + number; }
+                    }
+
+                    if (!error.Equals("Negative numbers:")) { throw new Exception(error); }
+                    return sum;
+                }
+            }
+
+            if (input.Contains(",") || input.Contains("\n"))
+            {
+                string[] arrNumbers = input.Split(new string[] { ",", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string number in arrNumbers) 
+                { 
+                    int num = int.Parse(number);
+                    if (num >= 0 && num < 1000) { sum += num; }
+                    else if (num < 0) { error += "\t" + number; }
+                }
+                
+                if (! error.Equals("Negative numbers:")) { throw new Exception(error); }
+                return sum;
+            }
+
+            else { sum = int.Parse(input); }
+
+            return sum;
+        }
+
+        
     }
+
 }
