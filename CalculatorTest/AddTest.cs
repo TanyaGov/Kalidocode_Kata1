f147c42cc3c31f5e2960c7c85016dc6cfa893b95
TanyaGovender <tcgovender2003@gmail.com>
2025-03-31 13:06:20 +0200

    Returns sum of numbers seperated by commas and next line

--- CalculatorTest/AddTest.cs
+++ CalculatorTest/AddTest.cs
@@ -46,5 +46,61 @@ namespace CalculatorTest
             Assert.That(sum, Is.EqualTo(92));
         }
 
+        [Test]
+        public void GivenTwoNumber_WhenAdded_ReturnsSum()
+        {
+            //Arrange
+            string input = "92,12";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(104));
+        }
+
+        [Test]
+        public void GivenCommas_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            string input = "33,44,55";
+            Calculator calculator = new Calculator();
+
+            // Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(132));
+        }
+
+        [Test]
+        public void GivenNextLine_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            string input = "33\n44\n55";
+            Calculator calculator = new Calculator();
+
+            // Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(132));
+        }
+
+        [Test]
+        public void GivenBothCommasAndNextLine_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            string input = "33\n44,55";
+            Calculator calculator = new Calculator();
+
+            // Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(132));
+        }
+
     }
 }
