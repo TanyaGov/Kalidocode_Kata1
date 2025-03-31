3ac52cd37c2ed0e422e67271545baf8520d35c9a
TanyaGovender <tcgovender2003@gmail.com>
2025-03-31 14:10:10 +0200

    Test Project renamed

---
+++ Kalidocode_Kata1Test/CalculatorTest.cs
@@ -0,0 +1,189 @@
+﻿using Kalidocode_Kata1;
+
+namespace Kalidocode_Kata1Test
+{
+    public class Tests
+    {
+        [Test]
+        public void GivenEmpty_WhenAdded_ReturnsZero()
+        {
+            //Arrange
+            string input = "";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GivenWhiteSpace_WhenAdded_ReturnsZero()
+        {
+            //Arrange
+            string input = " ";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GivenOneNumber_WhenAdded_ReturnsThatNumber()
+        {
+            //Arrange
+            string input = "92";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(92));
+        }
+
+        [Test]
+        public void GivenTwoNumbers_WhenAdded_ReturnsSum()
+        {
+            //Arrange
+            string input = "92,12";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(104));
+        }
+
+        [Test]
+        public void GivenCommas_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            string input = "33,44,55";
+            Calculator calculator = new Calculator();
+
+            // Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(132));
+        }
+
+        [Test]
+        public void GivenNextLine_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            string input = "33\n44\n55";
+            Calculator calculator = new Calculator();
+
+            // Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(132));
+        }
+
+        [Test]
+        public void GivenBothCommasAndNextLine_WhenAdded_ReturnsSum()
+        {
+            // Arrange
+            string input = "33\n44,55";
+            Calculator calculator = new Calculator();
+
+            // Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(132));
+        }
+
+        [Test]
+        public void GivenUnkownAmountOfNumbers_WhenAdded_ReturnsSumAllNumbers()
+        {
+            // Arrange
+            string input = "33,44,55,66,77,88,99,11,22";
+            Calculator calculator = new Calculator();
+
+            // Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(495));
+        }
+
+        [Test]
+        public void GivenSingleCustomDelimiter_WhenAdded_ReturnsSum()
+        {
+            //Arrange
+            string input = "//*\n19*4*16";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(39));
+        }
+
+        [Test]
+        public void GivenNegativeNumber_WhenAdded_ReturnsError()
+        {
+            //Arrange
+            string input = "33\n-44\n55";
+            Calculator calculator = new Calculator();
+
+            //Act 
+            Exception ex = Assert.Throws<Exception>(() => calculator.Add(input));
+
+            //Assert
+            Assert.That(ex.Message, Does.Contain("-44"));
+        }
+
+        [Test]
+        public void GivenMultipleNegativeNumbers_WhenAdded_ReturnsError()
+        {
+            //Arrange
+            string input = "33\n-22\n-44,55,66,-77";
+            Calculator calculator = new Calculator();
+
+            //Act 
+            Exception ex = Assert.Throws<Exception>(() => calculator.Add(input));
+
+            //Assert
+            Assert.That(ex.Message, Does.Contain("-22,-44,-77"));
+        }
+
+        [Test]
+        public void GivenNumberOver1000_WhenAdded_ReturnsSumWithout1000()
+        {
+            //Arrange
+            string input = "1111,22,88";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(110));
+        }
+
+        [Test]
+        public void GivenMultipleCustomDelimiters_WhenUsingAll_ReturnsSum()
+        {
+            //Arrange
+            string input = "//[***][&][^]\n4***30***6&10^5";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum, Is.EqualTo(55));
+        }
+    }
+}
