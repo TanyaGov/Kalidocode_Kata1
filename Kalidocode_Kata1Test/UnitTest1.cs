262495e01617e87e9dce847ab469380f07585bfa
TanyaGovender <tcgovender2003@gmail.com>
2025-04-01 10:09:56 +0200

    New Project created

---
+++ Kalidocode_Kata1Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Kalidocode_Kata1Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
