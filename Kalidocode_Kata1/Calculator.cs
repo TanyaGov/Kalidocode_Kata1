baad5dd192fbfaa7c39c69b7b89663494736c11d
TanyaGovender <tcgovender2003@gmail.com>
2025-04-01 10:28:21 +0200

    Empty strings return 0

---
+++ Kalidocode_Kata1/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace Kalidocode_Kata1
+{
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+            return Convert.ToInt32(input);
+        }
+    }
+}
---
+++ Kalidocode_Kata1Test/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using Kalidocode_Kata1;
+
+namespace Kalidocode_Kata1Test
+{
+    public class CalculatorTest
+    {
+        [Test]
+        public void GIVEN_EmptyString_WHEN_Added_THEN_ReturnsZero()
+        {
+            //Arrange
+            string input = " ";
+            Calculator calculator = new Calculator();
+
+            //Act
+            int sum = calculator.Add(input);
+
+            //Assert
+            Assert.That(sum,Is.EqualTo(0));
+        }
+    }
+}
