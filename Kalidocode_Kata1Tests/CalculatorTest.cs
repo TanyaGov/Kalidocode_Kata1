73ac2e0ed67e27fea2193b2b5444b1e47fdcb385
TanyaGovender <tcgovender2003@gmail.com>
2025-04-02 13:56:30 +0200

    Tests created for ConvertStringNumbers class

--- Kalidocode_Kata1Tests/CalculatorTest.cs
+++ Kalidocode_Kata1Tests/CalculatorTest.cs
@@ -1,8 +1,29 @@
-﻿
+﻿using System;
+using Kalidocode_Kata1;
 
 namespace Kalidocode_Kata1Tests
 {
     public class CalculatorTest
     {
+        public Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void GIVEN_CAdd_WHEN_Called_THEN_ReturnsConvertedNumber()
+        {
+            //Arrange
+            string input = "2";
+
+            //Act 
+            int result = converter.ConvertOneNumber(input);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
     }
 }
